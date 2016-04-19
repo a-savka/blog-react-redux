615d27567ec2c8d2c1243af9124bee11857e925e
Andrey Savka <a_sawka@yahoo.com>
2016-04-19 21:13:39 +0300

    validate new post form

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -1,33 +1,59 @@
-import React, { Component } from 'react';
+import React, { Component, PropTypes } from 'react';
 import { reduxForm } from 'redux-form';
 import { createPost } from '../actions/index';
+import { Link } from 'react-router';
 
 class PostsNew extends Component {
+
+  static contextTypes = {
+    router: PropTypes.object
+  };
+
+  getGroupClass(field) {
+    return `form-group ${ field.touched && field.invalid ? 'has-danger' : '' }`;
+  }
+
+  onSubmit(fields) {
+    this.props.createPost(fields)
+      .then(() => {
+        this.context.router.push('/')
+      })
+  }
 
   render() {
 
     const {fields: {title, categories, content}, handleSubmit} = this.props;
 
     return (
-      <form onSubmit={handleSubmit(this.props.createPost)}>
+      <form onSubmit={handleSubmit(this.onSubmit.bind(this))}>
         <h3>Creae A Blog Post</h3>
 
-        <div className="form-group">
+        <div className={this.getGroupClass(title)}>
           <label>Title</label>
           <input type="text" className="form-control" {...title}/>
+          <div className="text-help">
+            {title.touched ? title.error : ''}
+          </div>
         </div>
 
-        <div className="form-group">
+        <div className={this.getGroupClass(categories)}>
           <label>Categories</label>
           <input type="text" className="form-control" {...categories}/>
+          <div className="text-help">
+            {categories.touched ? categories.error : ''}
+          </div>
         </div>
 
-        <div className="form-group">
+        <div className={this.getGroupClass(content)}>
           <label>Content</label>
           <textarea className="form-control" {...content}/>
+          <div className='text-help'>
+            {content.touched ? content.error : ''}
+          </div>
         </div>
 
         <button type="submit" className="btn btn-primary">Submit</button>
+        <Link to="/" className="btn btn-danger">Back</Link>
 
       </form>
     );
@@ -36,8 +62,25 @@ class PostsNew extends Component {
 }
 
 
+function validate(values) {
+  let errors = {};
+
+  if(!values.title) {
+    errors.title = "Please enter a post title";
+  }
+  if(!values.categories) {
+    errors.categories = "Please enter categories";
+  }
+  if(!values.content) {
+    errors.content = "Please enter content";
+  }
+
+  return errors;
+}
+
 
 export default reduxForm({
   form: "PostsNewForm",
-  fields: ['title', 'categories', 'content']
+  fields: ['title', 'categories', 'content'],
+  validate
 }, null, { createPost })(PostsNew);
